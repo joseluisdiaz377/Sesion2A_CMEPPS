c87008ba7f470314811f71d642e183f4c39d29d2
usuario <usuario@pc21-096>
2023-10-03 16:47:10 +0200

    Añadiendo JUnit

---
+++ src/test/CalculadoraTest.java
@@ -0,0 +1,86 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pkg.Calculadora;
+
+class CalculadoraTest {
+	private int value1;
+	private int value2;
+	
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		
+		//ideal para comprobar la conexion con bases de datos y eso
+		
+		System.out.println("Before All");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		//para borrar datos de pruebas y eso
+		
+		System.out.println("After All");
+
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		System.out.println("Before Each");
+		value1 = 5;
+		value2 = 3;
+
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		
+		System.out.println("After Each");
+
+	}
+
+	@Test
+	void testSuma() {
+		int expected = 8;
+		int actual = Calculadora.suma(value1, value2);  //obtenido
+		assertEquals(expected, actual);
+		
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	void testResta() {
+		int expected = 2;
+		int actual = Calculadora.resta(value1, value2);
+		assertEquals(expected, actual);
+		
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultiplica() {
+		int expected = 15;
+		int actual = Calculadora.multiplica(value1, value2);
+		assertEquals(expected, actual);
+		
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	void testDivide() {
+		int expected = 5/3;
+		int actual = Calculadora.divide(value1, value2);
+		assertEquals(expected, actual);
+		
+		//fail("Not yet implemented");
+	}
+
+}
